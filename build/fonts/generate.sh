01ac336420e4739ddc6c253eed4fb9d0fa491be1
denmigda <denis.migdal@uca.fr>
2026-09-11 10:48:30 +0200

    force font for test

--- build/fonts/generate.sh
+++ build/fonts/generate.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "$0")
 function generate() {
     pyftsubset "$DIR/sources/$1.woff2" \
             --output-file="$DIR/generated/$1.woff2" --flavor=woff2 \
-            --unicodes="$2" 
+            --unicodes="$2"
 }
 
 # 🧩 U+1F9E9
