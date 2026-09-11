5c73179e9ffb27c1acf34f480137e28a7beee405
denmigda <denis.migdal@uca.fr>
2026-09-11 14:53:37 +0200

    add house to symbols

--- build/fonts/generate.sh
+++ build/fonts/generate.sh
@@ -8,4 +8,4 @@ function generate() {
             --unicodes="$2"
 }
 
-generate "NotoColorEmoji" "U+26A0,U+1F310,U+1F44B,U+1F4BB,U+1F4A1,U+1F4DA,U+1F510,U+1F5C3,U+1F6E0,U+1F9E9"
+generate "NotoColorEmoji" "U+26A0,U+1F310,U+1F3E0,U+1F44B,U+1F4BB,U+1F4A1,U+1F4DA,U+1F510,U+1F5C3,U+1F6E0,U+1F9E9"
